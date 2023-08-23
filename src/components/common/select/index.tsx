7d2fb219b1d264b76c6c1496fc042c2adb6ee776
Harshit Shukla <harshitcr7777@gmail.com>
2023-08-23 15:22:49 +0530

    Added remaining props of select

--- src/components/common/select/index.tsx
+++ src/components/common/select/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Select } from "antd";
 interface OptionsItems {
   value: string;
@@ -8,15 +8,25 @@ interface OptionsItems {
 interface SelectProps {
   options: OptionsItems[];
   className?: string;
-  placeholder?: string;
+  placeholder?: ReactNode;
+  value?: string;
+  id?: string;
 }
 
 const handleChange = (value: string) => {
   //   console.log(`selected ${value}`);
 };
 
-const select: React.FC<SelectProps> = ({ options, className , placeholder}) => (
+const select: React.FC<SelectProps> = ({
+  options,
+  className,
+  placeholder,
+  value,
+  id,
+}) => (
   <Select
+    id={id}
+    value={value}
     placeholder={placeholder}
     className={className}
     style={{ width: 120 }}
