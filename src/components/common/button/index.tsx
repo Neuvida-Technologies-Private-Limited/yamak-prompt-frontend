7ef4059fcd8d9b0499bccc029323fe8de9a93ae1
Harshit Shukla <harshitcr7777@gmail.com>
2023-08-26 23:59:24 +0530

    Resolved the comments

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -1,20 +1,18 @@
 import React, { ReactNode } from "react";
 import { Button } from "antd";
-import { SizeType } from "antd/es/config-provider/SizeContext";
 
 interface ButtonProps {
   icon?: ReactNode;
   name?: string;
   className?: string;
-  color?: string;
-  size:SizeType;
+  size:"small" | "middle" | "large";
   type: "link" | "text" | "default" | "primary" | "dashed"; 
   shape:"default" | "circle" | "round" | undefined;
   href?:string
 }
 
-const button: React.FC<ButtonProps> = ({ icon, name, className, color, size, type, shape, href}) => (
-  <Button className={className} icon={icon} color={color} size={size} type={type} shape={shape} href={href} >
+const button: React.FC<ButtonProps> = ({ icon, name, className, size, type, shape, href}) => (
+  <Button className={className} icon={icon} size={size} type={type} shape={shape} href={href} >
     {name}
   </Button>
 );
