11b21125f1381ade831dbda1e0047634895e77c2
Harshit Shukla <harshitcr7777@gmail.com>
2023-09-01 15:52:50 +0530

    added tag

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,3 +4,4 @@ export { default as Select } from './select';
 export { default as Sidebar } from './sidebar';
 export { default as Navbar } from './navbar';
 export { default as Search } from './search';
+export { default as Tag } from './tag';
