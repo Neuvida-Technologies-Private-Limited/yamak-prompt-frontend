082752d0cc1055aa41d8f496baa5ba68e2a633ef
Harshit Shukla <harshitcr7777@gmail.com>
2023-09-02 13:25:23 +0530

    Added the styled input/textarea component

---
+++ src/components/common/input/styledInput.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+interface StyledInputProps {
+  label: string;
+  placeholder: string;
+  className: string;
+}
+
+const styledInput: React.FC<StyledInputProps> = ({
+  label,
+  placeholder,
+  className,
+}) => {
+  return (
+    <TextField
+      id="outlined-textarea fullWidth"
+      label={label}
+      placeholder={placeholder}
+      className={className}
+      multiline
+      focused
+    />
+  );
+};
+
+export default styledInput;
