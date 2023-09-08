e5d5229b0be8df66b4ccf82d95850b2072f46248
Himanshu Bisht <himanshubisht313@gmail.com>
2023-09-08 11:41:58 +0530

    add library page cards

--- src/components/helpers/library/LibraryCard.tsx
+++ src/components/helpers/library/LibraryCard.tsx
@@ -3,7 +3,7 @@ import { FiCopy, FiHeart } from 'react-icons/fi';
 import { BiLike, BiDislike } from 'react-icons/bi';
 import { PiWarningBold } from 'react-icons/pi';
 import { CardConst } from 'utils/constants';
-import { Button } from 'components/common';
+import { Button, Text } from 'components/common';
 
 interface CardProps {
   heading: string;
@@ -19,24 +19,39 @@ const buttons = [
 ];
 
 const LibraryCard: React.FC<CardProps> = ({ heading, subHeading }) => {
+  const importPromptHandler = () => {};
+
   return (
-    <div className="flex flex-col bg-bgSecondary w-1/3 p-5 rounded-2xl">
-      <div className="font-poppins font-bold text-md pb-3">
-        <h2 className="text-black">{heading}</h2>
+    <div className="flex flex-col bg-white p-4 rounded-2xl shadow">
+      <div className="flex sm:flex-col md:flex-row sm:items-start md:justify-between gap-4 items-center mb-4">
+        <h2 className="text-black font-bold text-md ">{heading}</h2>
+        <Button
+          size="small"
+          type="default"
+          shape="default"
+          onClick={importPromptHandler}
+          className="!text-xs whitespace-nowrap bg-gray50 text-primary800 font-bold !p-2 !py-4 !rounded-xl border-0"
+          name="Import Prompt"
+        />
       </div>
-      <div className="font-raleway font-normal text-xs pb-3">
-        <h3 className="text-gray800">{subHeading}</h3>
-      </div>
-      <div className="flex justify-between">
-        {buttons.map(button => (
+      <h3 className="bg-red50 text-gray800 inline-block self-start mb-4 p-1 px-2 rounded-lg text-xs">
+        {subHeading}
+      </h3>
+      <Text
+        children="Human Written | Plagiarism Free | SEO Optimized Long-Form Article with Proper Outline"
+        className="text-xs text-black opacity-100 font-medium mb-4"
+      />
+      <div className="flex flex-wrap justify-start gap-2">
+        {buttons.map((button, index) => (
           <Button
+            key={`library-card-icon-${index}`}
             onClick={button.onclick}
             size={undefined}
             type={'default'}
             shape={undefined}
             icon={button.icon}
             name={button.name}
-            className="flex items-center text-xs justify-center font-raleway"
+            className="flex items-center text-xs justify-center"
           />
         ))}
       </div>
--- src/pages/library.tsx
+++ src/pages/library.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  LibraryCard,
   LibraryCardFilter,
   LibraryHeader,
   LibrarySearchArea,
@@ -13,7 +14,14 @@ const library: React.FC = () => {
         <LibraryCardFilter />
         <LibrarySearchArea />
       </div>
-      <div className="p-6">Library cards</div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 p-6 font-poppins bg-gray50">
+        {Array.from(Array(10)).map(() => (
+          <LibraryCard
+            heading="Human Written | 100% Unique | SEO Optimized Article"
+            subHeading="SEO/Writing"
+          />
+        ))}
+      </div>
     </div>
   );
 };
