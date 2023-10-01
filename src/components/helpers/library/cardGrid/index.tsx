9a3847a42ed6d68e5d0581478bbd86c0e3065fea
Himanshu Bisht <himanshubisht313@gmail.com>
2023-10-01 17:55:18 +0530

    empty library page created

--- src/components/helpers/library/cardGrid/index.tsx
+++ src/components/helpers/library/cardGrid/index.tsx
@@ -1,28 +1,26 @@
 import { LibraryCardItem as CardItem } from 'types';
-import { LibraryCard } from 'components/helpers';
+import { EmptyLibrary, LibraryCard } from 'components/helpers';
 
 const CardsGrid: React.FC<{ items: CardItem[] }> = ({ items }) => {
-  return (
+  return items.length === 0 ? (
+    <EmptyLibrary />
+  ) : (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6 font-poppins bg-gray50">
-      {items.length === 0 ? (
-        <p>No items...</p>
-      ) : (
-        items.map((item, index) => (
-          <LibraryCard
-            key={`library-card-item-${index}`}
-            title={item.title}
-            bookmarked={item.bookmarked}
-            is_public={item.is_public}
-            liked_by_user={item.liked_by_user}
-            likes_dislikes_count={item.likes_dislikes_count}
-            prompt_type={item.prompt_type}
-            sample_output={item.sample_output}
-            tags={item.tags}
-            user_message={item.user_message}
-            uuid={item.uuid}
-          />
-        ))
-      )}
+      {items.map((item, index) => (
+        <LibraryCard
+          key={`library-card-item-${index}`}
+          title={item.title}
+          bookmarked={item.bookmarked}
+          is_public={item.is_public}
+          liked_by_user={item.liked_by_user}
+          likes_dislikes_count={item.likes_dislikes_count}
+          prompt_type={item.prompt_type}
+          sample_output={item.sample_output}
+          tags={item.tags}
+          user_message={item.user_message}
+          uuid={item.uuid}
+        />
+      ))}
     </div>
   );
 };
